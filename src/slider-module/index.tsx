aee066467dac25216538f99190769232023c30e7
shivamkumar28 <skp42574@gmail.com>
2023-11-21 07:20:48 +0530

    remove unnecessary code

---
+++ src/slider-module/index.tsx
@@ -0,0 +1,8 @@
+import { View } from "react-native"
+import { styles } from "./style"
+
+const CustomSlider=()=>{
+    return <View style={styles.container}></View>
+}
+
+export default CustomSlider
